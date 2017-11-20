a1143bb2c787bf488a4189caf1b5e02d37c514f3
fallerd <fallerd@gmail.com>
2017-11-20 12:20:30 -0700

    Add files via upload

---
+++ Layer Groups to Smart Objects.jsx
@@ -0,0 +1,25 @@
+﻿#target Photoshop
+function main(){
+if(!documents.length) return;
+var doc = activeDocument;
+for(var c =0; c<doc.layers.length;c++){
+   doc.activeLayer= doc.layers[c];
+   doc.activeLayer.visible=false;
+    }
+for(var a=doc.layers.length-1;a > -1;a--){
+    doc.activeLayer= doc.layers[a];
+    if(doc.activeLayer.isBackgroundLayer) continue;
+     doc.activeLayer.visible=true;
+    createSmartObject();
+    doc.activeLayer.visible=false;
+    }
+doc.activeLayer.visible=true;
+}
+main();
+
+
+function createSmartObject()
+{
+    var idnewPlacedLayer = stringIDToTypeID( 'newPlacedLayer' );
+    executeAction(idnewPlacedLayer, undefined, DialogModes.NO);  //photoshop action manager
+}
